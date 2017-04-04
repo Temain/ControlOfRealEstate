07235aa005ea810e5d3bf170758db3d82df6ec63
Temain <asv.artm@gmail.com>
2017-04-04 09:25:07 +0300

    Authorize

--- Controllers/IllegalObjectsController.cs
+++ Controllers/IllegalObjectsController.cs
@@ -101,7 +101,7 @@ namespace ControlOfRealEstate.Controllers
             }
         }
 
-        [Authorize("Administrator")]
+        [Authorize(Roles = "Administrator")]
         [HttpGet("parse")]
         public IActionResult Parse()
         {
